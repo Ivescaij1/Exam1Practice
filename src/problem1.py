bd2de594225b3b2d18b3c7a7d4fab7c17f265d33
ivescai <caij1@rose-hulman.edu>
2019-06-11 16:37:53 +0800

    M2 done

--- src/problem1.py
+++ src/problem1.py
@@ -349,7 +349,7 @@ def problem1c(n):
            and the sum of the digits in 223092870 is 33.
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -363,6 +363,15 @@ def problem1c(n):
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 to 20 minutes.
     # ------------------------------------------------------------------
+
+    p1c_product = 1
+    for k in range(n - 1):
+        if is_prime(k + 2):
+            p1c_product = p1c_product * (k + 2)
+
+    p1c_sum = sum_of_digits(p1c_product)
+
+    return p1c_sum
 
 
 # ----------------------------------------------------------------------
